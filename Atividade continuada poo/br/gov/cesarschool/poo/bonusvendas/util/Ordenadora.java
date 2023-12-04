ebcfeb200d7d9369b5308470ea48a29d1b0e59a9
bela975 <isfsca@cesar.school>
2023-12-04 06:31:16 -0300

    adicionando o algoritimo de ordenação da lista dos objetos

--- Atividade continuada poo/br/gov/cesarschool/poo/bonusvendas/util/Ordenadora.java
+++ Atividade continuada poo/br/gov/cesarschool/poo/bonusvendas/util/Ordenadora.java
@@ -1,24 +1,39 @@
 package br.gov.cesarschool.poo.bonusvendas.util;
+import java.util.List;
+
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
 
 public class Ordenadora {
+	
+	private Ordenadora(){
+		
+	}
+	
+	public static void ordenar(Object[] lista, Comparador comp) {
 
+		 int tamanho =  lista.length;
+		 
+			for(int i=0; i <= (tamanho -1); i++){
+				for(int k=(i+1);k <= (tamanho -1); k++) {
+					//referenciando o metodo comparar presente na classe Comparador
+					if(comp.comparar(lista[i], lista[k]) > 0) {
+						Object temp = lista[i];
+						lista[i] = lista[k];
+						lista[k] = temp;
+	                 }
+                 }
+		    }
+     }
 }
-//*Esta classe não necessita ser instanciada para ser usada, portanto, o seu construtor
-//deve ser sem parâmetros e privado.
 
-// Deve ter um método público estático void ordenar(Object[] lista, Comparador
+//Algoritmo de ordenacao
+
+
+//*Esta classe não necessita ser instanciada para ser usada, portanto, o seu construtor
+//deve ser sem parâmetros e privado. OK
+
+//Deve ter um método público estático void ordenar(Object[] lista, Comparador
 //comp): Este método deve ordenar a lista “lista” recebida como parâmetro, decidindo
 //quando trocar objetos de lugar a partir do resultado do método comparar, invocado
 //do comparador, que recebe os dois objetos a serem comparados. O algoritmo de
-//ordenação segue abaixo.
-
-//Algoritmo de ordenacao
-ordenar(Object[] lista){
-for(i=0 ate (lista.tamanho-1)){
-for(k=(i+1) ate (lista.tamanho-1)){
-if lista[i] > lista[k] {
-	trocar lista[i] e lista[k] de posição;
-}
-}
-}
-}
+//ordenação segue abaixo.OK
